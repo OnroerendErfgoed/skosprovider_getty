873cb7702784e1be715c673bf4580e43908c9c2c
Koen Van Daele <koen_van_daele@telenet.be>
2014-09-30 21:29:29 +0200

    Update examples to also show the use of the label function.

--- examples/churches.py
+++ examples/churches.py
@@ -11,14 +11,20 @@ aat = AATProvider(metadata={'id': 'AAT'})
 
 churches = aat.get_by_id(300007466)
 
-lang = ['en', 'nl', 'es', 'de']
+lang = ['en', 'nl', 'es', 'de', 'fr']
 
-print('Labels')
-print('------')
+print('Label per language')
+print('------------------')
+for l in lang:
+    label = churches.label(l)
+    print(l + ' --> ' + label.language + ': ' + label.label + ' [' + label.type + ']')
+
+print('All Labels')
+print('----------')
 for l in churches.labels:
    print(l.language + ': ' + l.label + ' [' + l.type + ']')
 
-print('Notes')
-print('-----')
+print('All Notes')
+print('---------')
 for n in churches.notes:
     print(n.language + ': ' + n.note + ' [' + n.type + ']')
--- examples/flanders.py
+++ examples/flanders.py
@@ -9,16 +9,22 @@ from skosprovider_getty.providers import TGNProvider
 
 aat = TGNProvider(metadata={'id': 'TGN'})
 
-churches = aat.get_by_id(7018236)
+flanders = aat.get_by_id(7018236)
 
-lang = ['en', 'nl', 'es', 'de']
+lang = ['en', 'nl', 'es', 'de', 'fr']
+
+print('Label per language')
+print('------------------')
+for l in lang:
+    label = flanders.label(l)
+    print(l + ' --> ' + label.language + ': ' + label.label + ' [' + label.type + ']')
 
 print('Labels')
 print('------')
-for l in churches.labels:
+for l in flanders.labels:
    print(l.language + ': ' + l.label + ' [' + l.type + ']')
 
 print('Notes')
 print('-----')
-for n in churches.notes:
+for n in flanders.notes:
     print(n.language + ': ' + n.note + ' [' + n.type + ']')
