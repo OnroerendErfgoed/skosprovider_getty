51afae864cdebb64d462021e82e956205b740dac
Koen Van Daele <koen_van_daele@telenet.be>
2014-12-17 23:27:13 +0100

    Reenable tests.
    Problems should be solved. Thanks to the Getty.

--- tests/test_providers.py
+++ tests/test_providers.py
@@ -23,24 +23,24 @@ class GettyProviderTests(unittest.TestCase):
         self.assertEqual(concept['type'], 'concept')
         self.assertIsInstance(concept['labels'], list)
 
-        # preflabels = [{'nl': 'kerken'}, {'de': u'kirchen (Gebäude)'}]
-        # preflabels_conc = [{label['language']: label['label']} for label in concept['labels']
-        #                    if label['type'] == 'prefLabel']
-        # self.assertGreater(len(preflabels_conc), 0)
-        # for label in preflabels:
-        #     self.assertIn(label, preflabels_conc)
-        #
-        # altlabels = [{'nl': 'kerk'}, {'de': u'kirche (Gebäude)'}]
-        # altlabels_conc = [{label['language']: label['label']} for label in concept['labels']
-        #                   if label['type'] == 'altLabel']
-        # self.assertGreater(len(altlabels_conc), 0)
-        # for label in altlabels:
-        #     self.assertIn(label, altlabels_conc)
+        preflabels = [{'nl': 'kerken'}, {'de': u'kirchen (Gebäude)'}]
+        preflabels_conc = [{label['language']: label['label']} for label in concept['labels']
+                           if label['type'] == 'prefLabel']
+        self.assertGreater(len(preflabels_conc), 0)
+        for label in preflabels:
+            self.assertIn(label, preflabels_conc)
+       
+        altlabels = [{'nl': 'kerk'}, {'de': u'kirche (Gebäude)'}]
+        altlabels_conc = [{label['language']: label['label']} for label in concept['labels']
+                          if label['type'] == 'altLabel']
+        self.assertGreater(len(altlabels_conc), 0)
+        for label in altlabels:
+            self.assertIn(label, altlabels_conc)
 
         self.assertGreater(len(concept['notes']), 0)
 
         self.assertEqual(concept['id'], '300007466')
-        #self.assertEqual(concept['broader'][0], '300007391')
+        self.assertEqual(concept['broader'][0], '300007391')
         print(concept)
         self.assertIn('300312247', concept['related'])
 
@@ -50,8 +50,8 @@ class GettyProviderTests(unittest.TestCase):
         self.assertEqual(collection['uri'], 'http://vocab.getty.edu/aat/300007473')
         self.assertEqual(collection['type'], 'collection')
         self.assertIsInstance(collection['labels'], list)
-        # self.assertIn(u'<kerken naar vorm>', [label['label'] for label in collection['labels']
-        #                                      if label['language'] == 'nl' and label['type'] == 'prefLabel'])
+        self.assertIn(u'<kerken naar vorm>', [label['label'] for label in collection['labels']
+                                             if label['language'] == 'nl' and label['type'] == 'prefLabel'])
         self.assertEqual(len(collection['notes']), 0)
 
     def test_get_by_id_invalid(self):
@@ -84,8 +84,8 @@ class GettyProviderTests(unittest.TestCase):
         concept = TGNProvider({'id': 'TGN'}).get_by_id('1000063')
         concept = concept.__dict__
         self.assertEqual(concept['uri'], 'http://vocab.getty.edu/tgn/1000063')
-        # self.assertIn(u'België', [label['label'] for label in concept['labels']
-        #                          if label['language'] == 'nl' and label['type'] == 'prefLabel'])
+        self.assertIn(u'België', [label['label'] for label in concept['labels']
+                                 if label['language'] == 'nl' and label['type'] == 'prefLabel'])
 
     def test_get_all(self):
         self.assertFalse(TGNProvider({'id': 'TGN'}).get_all())
@@ -209,8 +209,6 @@ class GettyProviderTests(unittest.TestCase):
             self.assertIn('kerk', res['label'].lower())
             self.assertEqual(res['type'], 'Concept')
 
-
-
     def test_find_member_collections_in_collection(self):
         r = AATProvider({'id': 'AAT'}).find({'label': 'church', 'type': 'collection', 'collection': {'id': '300007466', 'depth': 'members'}})
         self.assertIsInstance(r, list)
@@ -227,6 +225,3 @@ class GettyProviderTests(unittest.TestCase):
         self.assertEqual(len(list_concept_subclasses), 4)
         list_collection_subclasses = subclasses.collect_subclasses(SKOS.Collection)
         self.assertEqual(len(list_collection_subclasses), 4)
-
-
-
