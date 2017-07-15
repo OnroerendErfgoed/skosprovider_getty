71c932a6efaadf1a685a52ba0b6e103935e40f5b
Koen Van Daele <koen.vandaele@rwo.vlaanderen.be>
2017-07-15 17:39:37 +0200

    Add an example of collecting subclasses. #28

---
+++ examples/collect_subclasses.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+This script demonstrates retrieving subclasses for a certain class.
+'''
+
+
+from skosprovider_getty.utils import SubClassCollector
+
+from skosprovider_getty.utils import GVP, ISO, SKOS
+
+sc = SubClassCollector(GVP)
+
+sc.collect_subclasses(SKOS.Concept)
+print(sc.get_subclasses(SKOS.Concept))
+
+sc.collect_subclasses(SKOS.Collection)
+print(sc.get_subclasses(SKOS.Collection))
+
+sc.collect_subclasses(SKOS.OrderedCollection)
+print(sc.get_subclasses(SKOS.OrderedCollection))
+
+sc.collect_subclasses(SKOS.OrderedCollection)
+print(sc.get_subclasses(SKOS.OrderedCollection))
+
+sc.collect_subclasses(ISO.ThesaurusArray)
+print(sc.get_subclasses(ISO.ThesaurusArray))
+
+skosc = SubClassCollector(SKOS)
+skosc.collect_subclasses(SKOS.Collection)
+print(skosc.get_subclasses(SKOS.Collection))
